52922ff05d3821a8774dd7825127d9aea81ef66f
SniAssia <assia.snissi@um6p.ma>
2026-04-01 15:25:13 +0000

    z

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // This forces the dev server to use a standard host 
+    // and helps resolve the WebSocket 'ws://' failure
+    host: '0.0.0.0',
+    port: 8080,
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws',
+    },
+    allowedHosts: 'all'
+  }
 })
